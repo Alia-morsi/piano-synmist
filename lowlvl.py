082b7580543fae173bf4f98b9d63255003ced351
Alia <alia.morsi@aucegypt.edu>
2024-02-24 17:39:30 +0100

    fixed bugs and tested insert, delete, and offset

--- lowlvl.py
+++ lowlvl.py
@@ -4,15 +4,12 @@ import math
 import numpy as np
 import copy
 
-NA_ONSET_idx = 0
-NA_DUR_idx = 1
 
 MIDLVL_INSERT = 1
 MIDLVL_ROLLBACK = 2
 MIDLVL_WRNG_PRED_INS = 3
 LABEL_PITCH = 0
 LABEL_VELOCITY = 20
-
 
 #We cannot have enough control as in the case of m-san's code to the temporal shifts first. 
 #In Akira's logic, the 'scheduling' happens with the timeshifts at the same time, making things
@@ -31,14 +28,14 @@ class lowlvl:
         #the labels would be maintained wrt to timeto or the tgt_na 
         #target = using the nearest giventime, get the timeto. In thoery, all existing score notes will
         #have a timefrom value in the safe zones.
-
         #TODO: if src is an empty note array, fail and exit
-        start = src_na[0][NA_ONSET_idx]
-        end = start + src_na[-1][NA_DUR_idx]
-        self.timefrom = np.linspace(start, end, int(math.ceil(end)-math.ceil(start))*20) #not sure of resolution.. 
-        self.timeto = self.timefrom.copy()
+
+        start = src_na['onset_sec'][0]
+        end = src_na['onset_sec'][-1]+ src_na['onset_sec'][-1]
+        self.time_from = np.linspace(start, end, int(math.ceil(end)-math.ceil(start))*20) #not sure of resolution.. 
+        self.time_to = self.time_from.copy()
         
-        self.onsets = [note[NA_ONSET_idx] for note in src_na]
+        self.onsets = src_na['onset_sec']
         self.src_na = src_na
         self.tgt_na = copy.deepcopy(self.src_na)
         self.label_na = np.zeros(0, dtype=label_na_fields)
@@ -49,20 +46,18 @@ class lowlvl:
         return
     
     def _label_note(self, start, end, lowlvl_label, midlvl_label):
-        #since there is no notearray to midiroll functionality in partitura, 
-        #and the notearray is literally a note array, we just remove the fields that dont make sense.
-        self.label_na.append((start, end-start, LABEL_PITCH, LABEL_VELOCITY, lowlvl_label, midlvl_label))
-        self.label_na.sort()
+        new_label = np.array([(start, end-start, LABEL_PITCH, LABEL_VELOCITY, lowlvl_label, midlvl_label)], dtype=self.label_na.dtype)
+        self.label_na = np.concatenate((new_label, self.label_na))
+        self.label_na.sort(order='onset_sec')
         return 
     
     def _shift_labels(self, time, offset):
         #shift all labels after time 'time' by offset s
         #only used in case we relax the requirement that changes have to be done from earliest to latest.
         #offset can be +ve or -ve.
-        labels_start = np.array([i['onset_sec'] for i in self.labels_na])
+        labels_start = self.label_na['onset_sec']
         nearestIdx = np.fabs([labels_start - time]).argmin() #should be the one greater but for now whatev.
-        self.labels_na[nearestIdx:]['onset_sec'] += offset
-        self.labels_na[nearestIdx:]['duration_sec'] += offset
+        self.label_na[nearestIdx:]['onset_sec'] += offset
         return
     
     #this in itself does not shift consequent pitches
@@ -75,12 +70,11 @@ class lowlvl:
         tgt_insertion_time = self.time_to[nearestIdx]
 
         #following the regular_na fields structure above..
-        na_note = (tgt_insertion_time, duration, 0, 0, pitch, velocity, 0, 0)
-        self.tgt_na.append(na_note)
-        self.tgt_na.sort(key = lambda x: x['start'])
+        new_note = np.array([(tgt_insertion_time, duration, 0, 0, pitch, velocity, 0, 0, 'none')], dtype=self.tgt_na.dtype)
+        self.tgt_na = np.concatenate((self.tgt_na, new_note))
 
+        self.tgt_na.sort(order='onset_sec')
         self._label_note(tgt_insertion_time, tgt_insertion_time +duration, midlvl_label, lowlvl_label)
-
         return
     
     def pitch_delete(self, src_time, pitch, midlvl_label, lowlvl_label):
@@ -89,28 +83,31 @@ class lowlvl:
 
         window = 0.05 #a window 50 ms before and after for trying to find the onset of the pitch in question.
 
-        #another approach could be to just get all instances of this pitch, and get the nearest one to the 
-        #expected time..
-        lowerbound_in_tgt_na = np.fabs(np.array([i['onset_sec'] for i in self.tgt_na]) - time_in_tgtna - 0.05).argmin()
-        upperbound_in_tgt_na = np.fabs(np.array([i['onset_sec'] for i in self.tgt_na]) - time_in_tgtna + 0.05).argmin()
+        #we could evade little time offset problems by:
+        #1. setting a tol. window around the src_time (which we do) which we check for all notes of the specified
+        # pitch and get the nearest one to the given time.
+        #2. find all notes of that pitch in the score and just choose the nearest one within a thresh.
+        lowerbound_in_tgt_na = np.fabs(np.array([i['onset_sec'] for i in self.tgt_na]) - (time_in_tgtna - window)).argmin()
+        upperbound_in_tgt_na = np.fabs(np.array([i['onset_sec'] for i in self.tgt_na]) - (time_in_tgtna + window)).argmin()
 
         found = False
-        note_options = np.array([])
-        for i in range(lowerbound_in_tgt_na, upperbound_in_tgt_na +1):
+        note_options = []
+        for i in range(lowerbound_in_tgt_na, upperbound_in_tgt_na+1):
             #find all notes with the corresponding pitch
-            if self.tgt_na[i].pitch == pitch:
-                note_options.append((self.tgt_na[i][0], i))
+            if self.tgt_na['pitch'][i] == pitch:
+                note_options.append((self.tgt_na['onset_sec'][i], i))
                 found = True
-
+        
         if found==False:
             print('pitch not found')  
         else:
             #find closest to the src_time from the list. should be at the top
-            note_idx = np.sort(note_options, key=lambda x: np.fabs(x[0] - src_time))[0]
-            note_start = self.tgt_na[note_idx][0]
-            note_end = note_start + self.tgt_na[note_idx][1]
+            note_start, note_idx = sorted(note_options, key=lambda x: np.fabs(x[0] - src_time))[0]
+            note_end = note_start + self.tgt_na['duration_sec'][note_idx]
+            
             #remove it
-            self.tgt_na.pop(note_idx)
+            self.tgt_na = self.tgt_na[~((self.tgt_na['onset_sec'] == self.tgt_na['onset_sec'][note_idx]) & 
+                                        (self.tgt_na['pitch'] == self.tgt_na['pitch'][note_idx]))]
 
             self._label_note(note_start, note_end, lowlvl_label, midlvl_label)          
         return
@@ -125,7 +122,7 @@ class lowlvl:
         #this is for wiggles that make sense, without the need to change time_from or time_to.
         return
     
-    #Since the notes themselves are passed on by the caller, the only purpose of go back
+    #Since the notes themselves are passed on by the caller, the only purpose of go_back
     #is to ensure that: 1 the labels are correct, and that the timefrom/time to makes sense, since we 
     #want to keep the perf. array and the labels hidden from the midlevel.
     #For timeto, if a rollback happens, it is treated as if it were a long silence inserted
@@ -137,8 +134,9 @@ class lowlvl:
     #each note, in comparision to the score portion rolled back, should have its own label
     #but for now we move on..
     def go_back(self, src_time, back_time, notes=[]):
+        #notes should be with the same dtype and fields as na
         #add an offset after src_time, which is presumably the note at which the playhead is
-        shift_duration = notes[:-1][0] + notes[:-1][1] #corresponding to onset + duration
+        shift_duration = notes['onset_sec'][-1] + notes['duration_sec'][-1] #corresponding to onset + duration
         self.offset(src_time, shift_duration, "rollback")
 
         #starting performance na time:
@@ -149,7 +147,10 @@ class lowlvl:
         #and sort
         for note in notes:
             note['onset_sec'] += time_in_tgtna
-            self.tgt_na.append()
+            #make sure that notes fits the dtype of self.tgt_na.dtype
+        
+        self.tgt_na = np.concatenate((self.tgt_na, notes))
+        self.tgt_na.sort(order='onset_sec') #this will also crash... fix.
         return
         
     def go_fwd(self):
@@ -165,7 +166,6 @@ class lowlvl:
         nearestIdx_in_tgt_na = np.fabs(np.array([i['onset_sec'] for i in self.tgt_na]) - time_in_tgtna).argmin()
 
         self.tgt_na[nearestIdx_in_tgt_na:]['onset_sec'] += offset_time
-        self.tgt_na[nearestIdx_in_tgt_na:]['offset_sec'] += offset_time
 
         self.time_to[nearestIdx_in_time:] += offset_time       
         self._shift_labels(src_time, offset_time)
